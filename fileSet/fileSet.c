e7a0297cf3a823adf6de818e56ee4714051029e1
John Morris <john.morris@crunchydata.com>
2022-10-16 17:31:06 -0700

    Started File sets

---
+++ fileSet/fileSet.c
@@ -0,0 +1,5 @@
+//
+// Created by John Morris on 10/16/22.
+//
+
+#include "fileSet.h"
---
+++ fileSet/fileSet.h
@@ -0,0 +1,8 @@
+//
+// Created by John Morris on 10/16/22.
+//
+
+#ifndef FILTER_FILESET_H
+#define FILTER_FILESET_H
+
+#endif //FILTER_FILESET_H
